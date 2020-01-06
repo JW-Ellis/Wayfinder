d92f0a6b0be589f365d92a444d0822855eca7aee
JW-Ellis <jwilliamellis@gmail.com>
2020-01-05 18:33:36 -0600

    Remove sass from gitignore

--- trailSearch.js
+++ trailSearch.js
@@ -5,10 +5,6 @@ require('dotenv').config();
 const hikeKey = process.env.HIKINGKEY;
 
 exports.randomTrail = async function search(zip, length) {
-  // test variables
-  // let lat = 40.027;
-  // let lon = -105.2519;
-  // let length = 0;
 
   let trailName;
   let trailSummary;
@@ -36,40 +32,7 @@ exports.randomTrail = async function search(zip, length) {
     const resultsLength = trailParse.length;
     const trailSelect = Math.floor(Math.random() * resultsLength);
     const trailRandom = trailParse[trailSelect];
-    console.log(trailRandom);
-
-    // Test Data
-    // const trailRandom = {
-    //   id: 7029252,
-    //   name: 'Apex-Enchanted Lollipop',
-    //   type: 'Featured Hike',
-    //   summary: 'This is one of the best hikes on the Front Range.',
-    //   difficulty: 'blue',
-    //   stars: 4.2,
-    //   starVotes: 58,
-    //   location: 'West Pleasant View, Colorado',
-    //   url:
-    //     'https://www.hikingproject.com/trail/7029252/apex-enchanted-lollipop',
-    //   imgSqSmall:
-    //     'https://cdn-files.apstatic.com/hike/7004559_sqsmall_1554245622.jpg',
-    //   imgSmall:
-    //     'https://cdn-files.apstatic.com/hike/7004559_small_1554245622.jpg',
-    //   imgSmallMed:
-    //     'https://cdn-files.apstatic.com/hike/7004559_smallMed_1554245622.jpg',
-    //   imgMedium:
-    //     'https://cdn-files.apstatic.com/hike/7004559_medium_1554245622.jpg',
-    //   length: 5.7,
-    //   ascent: 1179,
-    //   descent: -1179,
-    //   high: 7312,
-    //   low: 6172,
-    //   longitude: -105.2098,
-    //   latitude: 39.7162,
-    //   conditionStatus: 'Minor Issues',
-    //   conditionDetails:
-    //     'Muddy, Snowy, Icy: Packed snow for most of the Apex trail. Enchanted Forest section is mostly ice and traction is recommended.',
-    //   conditionDate: '2019-12-22 17:30:15',
-    // };
+    console.log(hpData);
 
     // Get trail json data
     trailName = trailRandom.name;
