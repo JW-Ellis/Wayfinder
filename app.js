96f2218e9a97d7e33cb12872a1b9211054654d6d
JW-Ellis <jwilliamellis@gmail.com>
2020-06-11 23:21:37 -0500

    add 500 error handeler

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
-const zipcodes = require('zipcodes');
-const fetch = require('node-fetch');
-const trailSearch = require('./trailSearch');
-const weatherSearch = require('./weatherSearch');
-require('dotenv').config();
+const zipcodes = require("zipcodes");
+const fetch = require("node-fetch");
+const trailSearch = require("./trailSearch");
+const weatherSearch = require("./weatherSearch");
+require("dotenv").config();
 
-app.set('view engine', 'ejs');
-app.use(express.static('img'));
-app.use(express.static('public'));
+app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.use(express.static("img"));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.get('/trail', async (req, res) => {
+app.get("/trail", async (req, res) => {
   let trailData;
   let forecast;
 
@@ -25,10 +26,16 @@ app.get('/trail', async (req, res) => {
     trailData = await trailSearch.randomTrail(zip, length);
     forecast = await weatherSearch.forecast(zip);
   } catch (e) {
-    console.log('Trail search error: ', e.message);
+    console.log("Trail search error: ", e.message);
+    res.redirect("index");
   }
 
-  res.render('trail', { trailData, forecast });
+  res.render("trail", { trailData, forecast });
 });
 
-app.listen(process.env.PORT, () => console.log('listening at 8000'));
+app.use(function(error, req, res, next) {
+  res.status(500);
+  res.render("index", { title: "500: Internal Server Error", error: error });
+});
+
+app.listen(process.env.PORT, () => console.log("listening at 8000"));
